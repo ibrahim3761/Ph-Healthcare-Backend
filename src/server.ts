06a72e738ebadd5970363f7385afe86bb1c2d5a3
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-08-17 14:31:49 +0600

    Super admin,Tester admin, Tester dcotor seeding added

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
 import app from "./app";
 import config from "./app/config";
 import { prisma } from "./app/lib/prisma";
+import { seedSuperAdmin, seedTesterAdmin, seedTesterDoctor } from "./app/utils/seed";
 
 const PORT = config.port;
 
 const main = async () => {
 	try {
 		await prisma.$connect();
+		await seedSuperAdmin(); // Seed the super admin user
+		await seedTesterAdmin(); // Seed the tester admin user
+		await seedTesterDoctor(); // Seed the tester doctor user
 		console.log("Connected to the database successfully.");
 		app.listen(PORT, () => {
 			console.log(`Server is running on port ${PORT}`);
-		});
+		}); 
 	} catch (error) {
 		console.error("Error starting the server:", error);
 		await prisma.$disconnect();
