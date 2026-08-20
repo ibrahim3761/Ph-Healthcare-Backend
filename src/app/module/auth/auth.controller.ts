b504c5aab20c27359940f336068b77924d8f9c32
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-08-20 18:59:13 +0600

    email-verification-added

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -12,7 +12,34 @@ const registerPatient = catchAsync(async (req: Request, res: Response) => {
 	//     throw new Error(payload.error.issues[0].message);
 	//   }
 	const payload = req.body;
-	const result = await AuthService.registerPatient(payload);
+	await AuthService.registerPatient(payload);
+
+	// const { accessToken, refreshToken, user, patient } = result;
+
+	// res.cookie("accessToken", accessToken, {
+	// 	httpOnly: true,
+	// 	secure: false,
+	// 	sameSite: "none",
+	// 	maxAge: 1000 * 60 * 60 * 24, // 24 hour or 1 day
+	// });
+	// res.cookie("refreshToken", refreshToken, {
+	// 	httpOnly: true,
+	// 	secure: false,
+	// 	sameSite: "none",
+	// 	maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+	// });
+
+	sendResponse(res, {
+		statusCode: httpStatus.CREATED,
+		success: true,
+		message: "Verification OTP sent",
+		data: null,
+	});
+});
+const verifyPatientEmail = catchAsync(async (req: Request, res: Response) => {
+
+	const payload = req.body;
+	const result = await AuthService.verifyPatientEmail(payload);
 
 	const { accessToken, refreshToken, user, patient } = result;
 
@@ -32,12 +59,12 @@ const registerPatient = catchAsync(async (req: Request, res: Response) => {
 	sendResponse(res, {
 		statusCode: httpStatus.CREATED,
 		success: true,
-		message: "Patient registered successfully",
+		message: "Verification OTP sent",
 		data: {
-			accessToken,
-			refreshToken,
 			user,
 			patient,
+			accessToken,
+			refreshToken,
 		},
 	});
 });
@@ -187,4 +214,5 @@ export const AuthController = {
 	googleLogin,
 	forgotPassword,
 	resetPassword,
+	verifyPatientEmail
 };
--- src/app/module/auth/auth.validation.ts
+++ src/app/module/auth/auth.validation.ts
@@ -1,56 +1,63 @@
 import z from "zod";
 
 const PatientRegistrationZodSchema = z.object({
-	name: z
-		.string()
-		.min(3, "Name must be at least 3 characters long")
-		.max(8, "Name must be at most 8 characters long"),
-	email: z.email("Invalid email address"),
-	password: z
-		.string()
-		.min(8, "Password must be at least 8 characters long")
-		.regex(/[a-z]/, "Password must contain atleast 1 Lowercase Letter")
-		.regex(/[A-Z]/, "Password must contain atleast 1 Uppercase Letter")
-		.regex(/[0-9]/, "Password must contain atleast 1 Number")
-		.regex(/[^A-Za-z0-9]/, "Password must contain atleast 1 Special Character"),
-	patient: z
-		.object({
-			contactNumber: z.string().optional(),
-		})
-		.optional(),
+  name: z
+    .string()
+    .min(3, "Name must be at least 3 characters long")
+    .max(8, "Name must be at most 8 characters long"),
+  email: z.email("Invalid email address"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .regex(/[a-z]/, "Password must contain atleast 1 Lowercase Letter")
+    .regex(/[A-Z]/, "Password must contain atleast 1 Uppercase Letter")
+    .regex(/[0-9]/, "Password must contain atleast 1 Number")
+    .regex(/[^A-Za-z0-9]/, "Password must contain atleast 1 Special Character"),
+  patient: z
+    .object({
+      contactNumber: z.string().optional(),
+    })
+    .optional(),
+});
+
+const PatientEmailVerifyZodSchema = z.object({
+  email: z.email("Not email!!"),
+  otp: z.string().length(6),
 });
 
 const LoginZodSchema = z.object({
-	email: z.email(),
-	password: z
-		.string()
-		.min(8, "Password Must Minimum 8 Characters Long.")
-		.regex(/[a-z]/, "Password must contain atleast 1 Lowercase Letter")
-		.regex(/[A-Z]/, "Password must contain atleast 1 Uppercase Letter")
+  email: z.email(),
+  password: z
+    .string()
+    .min(8, "Password Must Minimum 8 Characters Long.")
+    .regex(/[a-z]/, "Password must contain atleast 1 Lowercase Letter")
+    .regex(/[A-Z]/, "Password must contain atleast 1 Uppercase Letter")
 
-		.regex(/[0-9]/, "Password must contain atleast 1 Number")
-		.regex(/[^A-Za-z0-9]/, "Password must contain atleast 1 Special Character"),
+    .regex(/[0-9]/, "Password must contain atleast 1 Number")
+    .regex(/[^A-Za-z0-9]/, "Password must contain atleast 1 Special Character"),
 });
 
 const ForgotPasswordZodSchema = z.object({
-    email: z.email()
-})
+  email: z.email(),
+});
 
 const ResetPasswordZodSchema = z.object({
-    email: z.email(),
-    newPassword: z.string()
-        .min(8, "Password Must Minimum 8 Characters Long.")
-        .regex(/[a-z]/, "Password must contain atleast 1 Lowercase Letter")
-        .regex(/[A-Z]/, "Password must contain atleast 1 Uppercase Letter")
+  email: z.email(),
+  newPassword: z
+    .string()
+    .min(8, "Password Must Minimum 8 Characters Long.")
+    .regex(/[a-z]/, "Password must contain atleast 1 Lowercase Letter")
+    .regex(/[A-Z]/, "Password must contain atleast 1 Uppercase Letter")
 
-        .regex(/[0-9]/, "Password must contain atleast 1 Number")
-        .regex(/[^A-Za-z0-9]/, "Password must contain atleast 1 Special Character"),
-    otp : z.string().length(6)
-})
+    .regex(/[0-9]/, "Password must contain atleast 1 Number")
+    .regex(/[^A-Za-z0-9]/, "Password must contain atleast 1 Special Character"),
+  otp: z.string().length(6),
+});
 
 export const UserValidation = {
-	PatientRegistrationZodSchema,
-	LoginZodSchema,
-	ForgotPasswordZodSchema,
-	ResetPasswordZodSchema
+  PatientRegistrationZodSchema,
+  LoginZodSchema,
+  ForgotPasswordZodSchema,
+  ResetPasswordZodSchema,
+  PatientEmailVerifyZodSchema
 };
