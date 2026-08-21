884e1c18a4c15fcf2092b55232164689452b24a0
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-08-21 18:22:10 +0600

    intregated cloudinary to update userImage

---
+++ src/app/module/user/user.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import httpStatus from "http-status";
+import { UserService } from "./user.service";
+
+const uploadProfileImage = catchAsync(async (req: Request, res: Response) => {
+  console.log("req.file", req.file?.buffer);
+
+  if (!req.file) {
+    throw new Error("No File Provided.");
+  }
+
+  const userId = req.user?.userId;
+
+  const result = await UserService.uploadProfileImage(req.file?.buffer, userId as string);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Porfile pic uploaded successfully",
+    data: result,
+  });
+});
+
+export const UserController = {
+  uploadProfileImage,
+};
---
+++ src/app/module/user/user.route.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { UserController } from "./user.controller";
+import { upload } from "../../lib/multer";
+import { auth } from "../../middleware/checkAuth";
+import { Role } from "../../../generated/prisma/enums";
+
+const router = Router();
+
+router.patch("/profile-image", 
+    upload.single("profileImage"),auth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN),
+    UserController.uploadProfileImage);
+
+export const UserRoutes = router; 
